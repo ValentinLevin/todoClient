a8932d9124e930fb15fa3626dda3d2a197447db6
Valentin <levin_valentin@mail.ru>
2020-12-19 21:56:52 +0600

    20201219

--- src/main/java/kz/testcenter/todo/client/ToDoRestClient.java
+++ src/main/java/kz/testcenter/todo/client/ToDoRestClient.java
@@ -1,9 +1,18 @@
 package kz.testcenter.todo.client;
 
+import kz.testcenter.todo.client.domain.ToDo;
 import kz.testcenter.todo.client.error.ToDoErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Service
 public class ToDoRestClient {
@@ -15,6 +24,19 @@ public class ToDoRestClient {
         this.properties = properties;
         this.restTemplate = new RestTemplate();
         this.restTemplate.setErrorHandler(new ToDoErrorHandler());
+    }
+
+    public Iterable<ToDo> findAll() throws URISyntaxException {
+        RequestEntity<Iterable<ToDo>> requestEntity =
+                new RequestEntity<>(HttpMethod.GET, new URI(properties.getUrl() + properties.getBasePath() + "/todo"));
+
+        ResponseEntity<Iterable<ToDo>> response =
+                restTemplate.exchange(requestEntity, new ParameterizedTypeReference<Iterable<ToDo>>(){});
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        }
+
+        return null;
     }
 
 }
--- src/main/java/kz/testcenter/todo/client/TodoClientApplication.java
+++ src/main/java/kz/testcenter/todo/client/TodoClientApplication.java
@@ -1,8 +1,11 @@
 package kz.testcenter.todo.client;
 
+import kz.testcenter.todo.client.domain.ToDo;
 import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Log
@@ -11,4 +14,12 @@ public class TodoClientApplication {
         SpringApplication.run(TodoClientApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner process(ToDoRestClient client) {
+        return args -> {
+            Iterable<ToDo> toDos = client.findAll();
+            assert toDos != null;
+            toDos.forEach(toDo -> log.info(toDo.toString()));
+        };
+    }
 }
